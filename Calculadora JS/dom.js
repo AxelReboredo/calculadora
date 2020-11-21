005f7dfdb314f2eec91b24a23f07f6ae6d5d2a06
AxelReboredo <72027859+AxelReboredo@users.noreply.github.com>
2020-11-21 20:53:26 -0300

    Add files via upload

---
+++ Calculadora JS/dom.js
@@ -0,0 +1,26 @@
+// MANEJO DEL DOM
+/* console.log(document.title);
+document.title = "COMPUTADORA"
+console.log(document.title);
+console.log(document.body);
+var h1 = document.getElementsByTagName("h1");
+// Me mustra los elementos que quiera ver
+console.log(h1);
+// Muestro con Console.log
+// Seleccionamos y podemos cambiar
+h1[0].innerHTML = "Prueba"
+// Con document.x agarramos elentos del DOM
+
+var boton = document.getElementById("boton");
+console.log(boton);
+
+boton.addEventListener("click", mensaje);
+function mensaje() {
+    alert("mensaje");
+}
+*/
+
+// CONSTRUCCION DE LA CALCULADORA
+
+
+
